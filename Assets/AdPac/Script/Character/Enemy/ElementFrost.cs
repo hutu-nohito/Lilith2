7689857e159da2f86e22db838e83f6d28d399afb
hutu-nohito <d0nte0sa@gmail.com>
2015-12-13 20:57:02 +0900

    float 移動できた

---
+++ Assets/AdPac/Script/Character/Enemy/ElementFrost.cs
@@ -0,0 +1,445 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementFrost : MonoBehaviour {
+
+
+    /*
+
+
+        (Serch) → Player → (Attack) → 距離10m以下 → 固有攻撃 　→  距離　→　yes → Serchへ
+                               ↓                                20m以上
+                           距離10m以上      →   ワープ回り込み →  Attackへ
+
+    */
+
+    //この辺は敵によりけりなのでこっち
+    public GameObject[] Bullet;//攻撃
+    public Transform[] Muzzle;//攻撃が出てくる場所
+
+    //ワープ時のフェード用
+    public GameObject Model;//モデル
+    private bool isFade = true;//魔方陣フェード用
+    private float color = 1;//透明度
+
+    public int Enemy_Level = 0;//この値によって攻撃方法を変える
+    /*
+
+        1:  Bubble
+        2:  Crescent
+        3:  Icicle
+
+        */
+
+    private Enemy_ControllerZ ecZ;
+    private MoveSmooth MS;//移動は全部これ
+    private Camera_ControllerZ CCZ;
+
+    private Animator animator;//アニメーションはふよふよと攻撃モーションのみ
+    private AudioSource[] SE;//音
+
+    //汎用
+    private float time = 0;//使ったら戻す
+    private Coroutine coroutine;//一度に動かすコルーチンは1つ ここでとっとけば止めるのが楽
+    private bool isCoroutine = false;//コルーチンを止めるときにはfalseに戻すこと
+
+
+    private int priority = 0;//状態の優先度
+    /*
+    Attack    1
+    isDamage  2
+    isReturn  3
+    isFind    4
+    Search    5
+    */
+
+    //キャラクタ固有の状態
+    //アニメーションは基本ここで管理するのでこれを使うときは向こうはIdleにでもしとく
+    public enum Element_State
+    {
+        Attack,//攻撃
+        Search,//うろうろ
+        Damage,
+        Return,
+    }
+    public Element_State state = Element_State.Search;
+    public Element_State GetState() { return state; }
+    public void SetState(Element_State state) { this.state = state; }
+
+    private Element_State oldstate = Element_State.Search;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        ecZ = GetComponent<Enemy_ControllerZ>();
+        MS = GetComponent<MoveSmooth>();
+        //animator = GetComponentInChildren<Animator>();
+        //SE = GetComponent<AudioSource>();
+
+        CCZ = Camera.main.gameObject.GetComponent<Camera_ControllerZ>();
+
+        priority = 5;//最初はサーチに
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (state == Element_State.Attack)
+        {
+            priority = 1;
+
+            //前を向ける
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(ecZ.Player.transform.position - transform.position), 0.05f);
+            transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+            if ((ecZ.Player.transform.position - transform.position).magnitude > 20)
+            {
+                coroutine = StartCoroutine(Warp());
+            }
+            else
+            {
+                coroutine = StartCoroutine(Attack());
+            }
+
+        }
+
+        if (ecZ.isDamage)
+        {
+            if (priority >= 2)
+            {
+                state = Element_State.Damage;
+                priority = 2;
+            }
+
+        }
+        if (state == Element_State.Damage)
+        {
+
+            if (priority >= 6)
+            {
+
+                priority = 2;
+
+                //アニメーションセット
+                //animator.SetTrigger("Kyorokyoro");//ここできょろきょろ                  
+
+                //前を向ける
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(ecZ.Player.transform.position - transform.position), 0.05f);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+            }
+            else if (priority >= 2)
+            {
+
+                priority = 2;
+                //アニメーションセット
+                //animator.SetTrigger("Walk");//歩き
+
+                //前を向ける
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(ecZ.Player.transform.position - transform.position), 0.05f);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+                //ダメージ受けたらだいたい死ぬが、生き残ったらとりあえず攻撃を激しくする
+
+                /*
+                ecZ.Move(ecZ.Player.transform.position, ecZ.speed);//Playerに近づく
+
+                //プレイヤとの距離で行動変化
+                if ((ecZ.Player.transform.position - transform.position).magnitude < 5)//距離が5以下だったら
+                {
+                    state = Fatbat_State.Attack;
+                    priority = 1;
+                }
+                if ((ecZ.Player.transform.position - transform.position).magnitude > 20)//距離が10以上だったら
+                {
+                    state = Fatbat_State.Syobon;
+                    priority = 4;
+                }
+                */
+            }
+        }
+
+        if (ecZ.isReturn)
+        {
+            if (priority >= 3)
+            {
+                state = Element_State.Return;
+                priority = 3;
+            }
+
+        }
+        if (state == Element_State.Return)
+        {
+
+            if (priority >= 3)
+            {
+                priority = 3;
+                //アニメーションセット
+                //animator.SetTrigger("Walk");//歩き
+
+                //とりあえず中心へ(Territoryはワールド座標にしとく)
+                ecZ.Move(ecZ.Territory.position, ecZ.speed);
+
+                //前を向ける
+                transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(ecZ.Territory.localPosition - transform.localPosition), 0.05f);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+                //テリトリとの距離で行動変化
+                if ((ecZ.Territory.localPosition - transform.localPosition).magnitude < 5)//真ん中ら辺まで戻ったら
+                {
+                    state = Element_State.Search;
+                    priority = 5;
+                }
+
+            }
+
+        }
+
+        if (ecZ.isFind)
+        {
+            if (priority >= 4)
+            {
+                state = Element_State.Attack;
+            }
+        }
+
+        if (state == Element_State.Search)
+        {
+            if (priority >= 5)
+            {
+                priority = 5;
+                //アニメーションセット
+                //animator.SetTrigger("Walk");//歩き
+
+                //前を向ける
+                transform.rotation = Quaternion.Slerp(transform.localRotation, Quaternion.LookRotation(ecZ.move_controller.End - transform.localPosition), 0.05f);
+                transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+                MS.Move(ecZ.move_controller.End, ecZ.speed);
+
+            }
+
+        }
+
+        if (isFade)
+        {
+            if (color < 1) color += Time.deltaTime * 1;
+            //Model.GetComponent<SkinnedMeshRenderer>().materials[0].color = new Color(Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.r, Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.g, Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.b, color);
+            Model.GetComponent<Renderer>().material.color = new Color(Model.GetComponent<Renderer>().material.color.r, Model.GetComponent<Renderer>().material.color.g, Model.GetComponent<Renderer>().material.color.b, color);
+
+        }
+        else
+        {
+            if (color > 0) color -= Time.deltaTime * 1;
+            //Model.GetComponent<SkinnedMeshRenderer>().materials[0].color = new Color(Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.r, Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.g, Model.GetComponent<SkinnedMeshRenderer>().materials[0].color.b, color);
+            Model.GetComponent<Renderer>().material.color = new Color(Model.GetComponent<Renderer>().material.color.r, Model.GetComponent<Renderer>().material.color.g, Model.GetComponent<Renderer>().material.color.b, color);
+        }
+
+        //状態が変化したら前の状態のいどうは中断
+        if (oldstate != state)
+        {
+            time = 0;
+            MS.Stop();
+            ecZ.Stop();
+        }
+        oldstate = state;
+
+    }
+
+    IEnumerator Warp()
+    {
+
+        if (isCoroutine) yield break;
+        isCoroutine = true;
+        ecZ.Reverse_Magic();
+
+        isFade = false;
+
+        yield return new WaitForSeconds(2);//消えるまで
+
+        transform.position = ecZ.Player.transform.position + new Vector3(0,ecZ.jump,0) + ecZ.Player.transform.TransformDirection(Vector3.back) * 5;//ﾌﾟﾚｲﾔの背後にワープ
+
+        yield return new WaitForSeconds(1);//移動
+
+        isFade = true;
+
+        yield return new WaitForSeconds(2);//現れるまで
+
+        state = Element_State.Attack;//Attack状態に戻す
+        priority = 1;
+        ecZ.Reverse_Magic();
+        isCoroutine = false;
+
+    }
+
+        IEnumerator Attack()
+    {
+
+        if (isCoroutine) yield break;
+        isCoroutine = true;
+        ecZ.Reverse_Magic();
+
+        yield return new WaitForSeconds(1);//ちょっと間をおいてから攻撃
+
+        GameObject bullet;
+
+        //アニメーションセット
+        //animator.SetTrigger("Attack");//攻撃
+
+
+        bullet = GameObject.Instantiate(Bullet[0]);//通常弾　flame
+        bullet.GetComponent<Attack_Parameter>().Parent = this.gameObject;//誰が撃ったかを渡す
+                                                                         //弾を飛ばす処理
+        bullet.transform.position = Muzzle[0].position;//Muzzleの位置
+        bullet.transform.rotation = Quaternion.LookRotation(ecZ.direction);//回転させて弾頭を進行方向に向ける
+
+        bullet.GetComponent<Rigidbody>().velocity = (ecZ.Player.transform.position - transform.position).normalized * bullet.GetComponent<Attack_Parameter>().speed;//ﾌﾟﾚｲﾔに向けて撃つ
+
+        Destroy(bullet, bullet.GetComponent<Attack_Parameter>().GetA_Time());
+
+        //効果音と演出
+        /*if (!SE[0].isPlaying)
+        {
+
+            SE[0].PlayOneShot(SE[0].clip);//SE
+
+        }*/
+
+        yield return new WaitForSeconds(bullet.GetComponent<Attack_Parameter>().GetR_Time());//撃った後の硬直
+
+
+        //Bomb
+        if (Enemy_Level == 1)
+        {
+
+            bullet = GameObject.Instantiate(Bullet[1]);//弾生成
+            bullet.GetComponent<Attack_Parameter>().Parent = this.gameObject;//もらった親を渡しておく必要がある
+
+            //弾を飛ばす処理
+            bullet.transform.position = Muzzle[1].position;//Muzzleの位置
+            bullet.transform.rotation = Quaternion.LookRotation(ecZ.direction);//回転させて弾頭を進行方向に向ける
+
+            bullet.GetComponent<Rigidbody>().velocity = ((ecZ.Player.transform.position - transform.position).normalized + transform.TransformDirection(new Vector3(0, 1, -0.3f))) * bullet.GetComponent<Attack_Parameter>().speed;//キャラの向いてる方向
+            Debug.Log(bullet.GetComponent<Rigidbody>().velocity);
+
+            //効果音と演出
+            /*if (!SE[1].isPlaying)
+            {
+
+                SE[1].PlayOneShot(SE[1].clip);//SE
+
+            }*/
+
+            Destroy(bullet, bullet.GetComponent<Attack_Parameter>().GetA_Time());
+
+            yield return new WaitForSeconds(bullet.GetComponent<Attack_Parameter>().GetR_Time());//撃った後の硬直
+
+        }
+
+        //Kanketusen
+        if (Enemy_Level == 2)
+        {
+
+            Vector3 OldPlayerPos = ecZ.Player.transform.position;
+
+            yield return new WaitForSeconds(Bullet[2].GetComponent<Attack_Parameter>().GetR_Time());//撃つ前の硬直
+
+            bullet = GameObject.Instantiate(Bullet[2]);//弾生成
+
+            bullet.GetComponent<Attack_Parameter>().Parent = this.gameObject;//もらった親を渡しておく必要がある
+
+            //弾を飛ばす処理
+
+            //足元を見る
+            RaycastHit hit;
+            GameObject hitObject;
+
+            Vector3 LineStart = new Vector3(OldPlayerPos.x, OldPlayerPos.y, OldPlayerPos.z);
+            Vector3 LineDirection = Vector3.down;//下でおｋ
+
+            if (Physics.Raycast(LineStart, LineDirection, out hit, 50))
+            {
+                hitObject = hit.collider.gameObject;//レイヤーがIgnoreLayerのObjectは弾かれる。
+
+                //Debug.DrawLine(LineStart, hit.point, Color.blue);
+                //Debug.Log(hitObject);
+
+                //地面だったら
+                if (hitObject.gameObject.name == "Terrain")
+                {
+
+                    bullet.transform.position = hit.point;
+
+                }
+            }
+            else
+            {
+                bullet.transform.position = new Vector3(OldPlayerPos.x, OldPlayerPos.y - ecZ.Player.transform.localScale.y / 2, OldPlayerPos.z);//下に地面がなかったら
+            }
+
+            bullet.transform.rotation = Quaternion.LookRotation(ecZ.direction);//回転させて弾頭を進行方向に向ける
+
+            //効果音と演出
+            /*if (!SE[2].isPlaying)
+            {
+
+                SE[2].PlayOneShot(SE[2].clip);//SE
+
+            }*/
+
+            Destroy(bullet, bullet.GetComponent<Attack_Parameter>().GetA_Time());
+
+            yield return new WaitForSeconds(0.5f);//撃った後の硬直
+        }
+
+        //Meteor
+        if (Enemy_Level == 3)
+        {
+            //animator.SetTrigger("Shoot");
+
+            yield return new WaitForSeconds(1);//撃つまでのため
+
+            CCZ.flag_quake = true;//カメラ揺らす
+
+            GameObject[] bulletM = new GameObject[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                bulletM[i] = GameObject.Instantiate(Bullet[3]);//弾生成
+
+                if (bulletM[i] != null)
+                {
+
+                    bulletM[i].GetComponent<Attack_Parameter>().Parent = this.gameObject;//もらった親を渡しておく必要がある
+
+                    bulletM[i].transform.position = Muzzle[3].position + transform.TransformDirection(new Vector3(Random.Range(-5, 6), Random.Range(0, 5), Random.Range(0, 15)));//ランダム
+                    bulletM[i].transform.rotation = Quaternion.LookRotation(transform.TransformDirection(Vector3.forward));//回転させて弾頭を進行方向に向ける
+                    bulletM[i].GetComponent<Rigidbody>().velocity = ((transform.TransformDirection(new Vector3(0, -4, 2))) * Bullet[3].GetComponent<Attack_Parameter>().speed);
+
+                    Destroy(bulletM[i], Bullet[3].GetComponent<Attack_Parameter>().GetA_Time());
+
+                }
+
+
+            }
+
+            yield return new WaitForSeconds(Bullet[3].GetComponent<Attack_Parameter>().GetR_Time());//撃った後の硬直
+
+            CCZ.flag_quake = false;//揺れを止める
+        }
+
+
+        ecZ.Reverse_Magic();
+        if ((ecZ.Player.transform.position - transform.position).magnitude > 30)//距離が20以上だったら
+        {
+            state = Element_State.Search;//Search状態に戻す
+            priority = 5;
+        }
+
+        isCoroutine = false;
+
+    }
+}
